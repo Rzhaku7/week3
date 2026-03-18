22ebe52f3c8d5c7227266e72f06506efcea76148
Rzhaku7 <rz32498@seeu.edu.mk>
2026-03-18 14:11:30 +0100

    dummy

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log("Hello from SEEU")
 //1.git init
 //2.git add remore origin
 //3 git branch -m main 
+//pushed the code on the main broser
+const dummy = ()=>
+{
+    console.log("dummy")
+}
